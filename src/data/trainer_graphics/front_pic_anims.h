43e644f53d9d66d4dfce2561c0383153de2e7c63
WennoBoi <wenpanik@gmail.com>
2019-09-04 16:25:04 +0200

    added trainer sprite custom1_front (untested)

--- src/data/trainer_graphics/front_pic_anims.h
+++ src/data/trainer_graphics/front_pic_anims.h
@@ -369,6 +369,10 @@ static const union AnimCmd *const sAnims_RubySapphireBrendan[] ={
 static const union AnimCmd *const sAnims_RubySapphireMay[] ={
     sAnim_GeneralFrame0,
 };
+//custom
+static const union AnimCmd *const sAnims_Custom1[] ={
+    sAnim_GeneralFrame0,
+};
 
 const union AnimCmd *const *const gTrainerFrontAnimsPtrTable[] =
 {
@@ -465,4 +469,5 @@ const union AnimCmd *const *const gTrainerFrontAnimsPtrTable[] =
     [TRAINER_PIC_LEAF] = sAnims_Leaf,
     [TRAINER_PIC_RS_BRENDAN] = sAnims_RubySapphireBrendan,
     [TRAINER_PIC_RS_MAY] = sAnims_RubySapphireMay,
+    [TRAINER_PIC_CUSTOM1] = sAnims_Custom1, //custom
 };
